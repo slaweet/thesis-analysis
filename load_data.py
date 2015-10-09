4f18e77830266acb3e1661a4ca699f3a56ef2d5b
Vít Stanislav <slaweet@seznam.cz>
2015-10-09 14:46:49 +0200

    Add more caching

--- load_data.py
+++ load_data.py
@@ -61,7 +61,24 @@ def get_answers_with_flashcards_and_orders(options):
     return answers_with_flashcards
 
 
+def get_cached(fn, options):
+    directory = '.cache'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_name = directory + '/' + fn.__name__ + options.answers.replace('/', '_') + '.pdy'
+    if os.path.isfile(file_name) and not options.no_cache:
+        data = pd.read_pickle(file_name)
+    else:
+        data = fn(options)
+        data.to_pickle(file_name)
+    return data
+
+
 def get_answers_with_flashcards_and_context_orders(options):
+    return get_cached(_get_answers_with_flashcards_and_context_orders, options)
+
+
+def _get_answers_with_flashcards_and_context_orders(options):
     answers_with_flashcards = get_answers_with_flashcards(options)
     answers_with_flashcards = answers_with_flashcards.sort(
         ['user_id', 'context_id', 'time'], ascending=True)
--- main.py
+++ main.py
@@ -50,6 +50,10 @@ def main():
     p.add_option("-u", "--use_cached_data", action="store_true",
                  dest="use_cached_data", default=None,
                  help="use data from last run of this command")
+
+    p.add_option("-n", "--no_cache", action="store_true",
+                 dest="no_cache", default=None,
+                 help="don't use data cached by functions in load_data.py")
     options, arguments = p.parse_args()
 
     possible_commands = dict([
